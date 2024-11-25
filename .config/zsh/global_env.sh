4f120c6cebd3bfa5db31020ad7ea428274ecffc6
purarue <7804791+purarue@users.noreply.github.com>
2024-11-24 21:52:23 -0800

    add tree sitter cli

--- .config/zsh/global_env.sh
+++ .config/zsh/global_env.sh
@@ -56,5 +56,6 @@ export DISCOGS_CACHE_DIR="${FILES_DIR}/discogs_cache"
 # https://github.com/purarue/offline_listens
 export OFFLINE_LISTENS_COMMANDS='offline-listens-source'
 export OFFLINE_LISTENS_FILE="${HPIDATA}/offline_listens/${ON_OS}.yaml"
+export ABOOK_FILE="${XDG_DOCUMENTS_DIR}/addressbook"
 
 export NVIM_SPELLFILE="${XDG_DOCUMENTS_DIR}/.nvim_spelldir/en.utf-8.add"
