434d553006170af37250af393b035f7dbdd27da6
purarue <7804791+purarue@users.noreply.github.com>
2025-07-21 14:07:35 -0700

    sync android camera directly isntead of multiple syncs

--- .config/zsh/android.zsh
+++ .config/zsh/android.zsh
@@ -23,12 +23,6 @@ evry 1 hour -sync_hpi_config && sync_hpi_config
 
 # reset any periodic syncs and re-run bgproc jobs
 syncfiles() {
-	for tag in \
-		twistytimer \
-		aw-window \
-		backup_images; do
-		rm -f "$(evry location -"$tag")"
-	done
 	sync_hpi_config
 	uqf
 }
