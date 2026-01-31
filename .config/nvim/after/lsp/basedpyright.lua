7cbb7c440d553bba5cd4e159efaa5c71ccb4158e
purarue <7804791+purarue@users.noreply.github.com>
2026-01-31 10:23:54 -0800

    nvim/python: disable some pyright disagnostics

--- .config/nvim/after/lsp/basedpyright.lua
+++ .config/nvim/after/lsp/basedpyright.lua
@@ -2,8 +2,16 @@ return {
     settings = {
         basedpyright = {
             analysis = {
+                diagnosticSeverityOverrides = {
+                    reportAny = false,
+                    reportUnusedCallResult = false,
+                    reportUnknownParameterType = false,
+                    reportUnknownVariableType = false,
+                    reportExplicitAny = false,
+                    reportPrivateLocalImportUsage = false,
+                },
                 autoSearchPaths = true,
-                -- typeCheckingMode = "recommended",
+                typeCheckingMode = "recommended",
                 useLibraryCodeForTypes = true,
                 diagnosticMode = "openFilesOnly",
                 inlayHints = {
