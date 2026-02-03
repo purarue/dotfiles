b4527217b5c4564a2e4e01522cdd99141bf6209e
purarue <7804791+purarue@users.noreply.github.com>
2026-02-02 22:00:54 -0800

    nvim: add csvview

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -8,6 +8,38 @@ return {
         keys = { { "<leader>ut", "<Cmd>UndotreeToggle<CR>", desc = "undotree" } },
     },
     {
+        "hat0uma/csvview.nvim",
+        ---@module "csvview"
+        ---@type CsvView.Options
+        opts = {
+            parser = { comments = { "#", "//" } },
+            keymaps = {
+                -- Text objects for selecting fields
+                textobject_field_inner = { "if", mode = { "o", "x" } },
+                textobject_field_outer = { "af", mode = { "o", "x" } },
+                -- Excel-like navigation:
+                -- Use <Tab> and <S-Tab> to move horizontally between fields.
+                -- Use <Enter> and <S-Enter> to move vertically between rows and place the cursor at the end of the field.
+                -- Note: In terminals, you may need to enable CSI-u mode to use <S-Tab> and <S-Enter>.
+                jump_next_field_end = { "<Tab>", mode = { "n", "v" } },
+                jump_prev_field_end = { "<S-Tab>", mode = { "n", "v" } },
+                jump_next_row = { "<Enter>", mode = { "n", "v" } },
+                jump_prev_row = { "<S-Enter>", mode = { "n", "v" } },
+            },
+        },
+        keys = {
+            {
+                "<leader>pc",
+                function()
+                    vim.cmd("CsvViewToggle")
+                    vim.print("Toggling CSV rendering...")
+                end,
+                desc = "toggle CSV rendering",
+            },
+        },
+        cmd = { "CsvViewEnable", "CsvViewDisable", "CsvViewToggle" },
+    },
+    {
         "catppuccin/nvim",
         name = "catppuccin",
         -- colorscheme, 1000 makes things load early
